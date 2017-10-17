6c88f789242c948b0964699ff15774c5a738e67e
zengjw <zengjw@vless.net>
2017-10-17 09:59:16 +0800

    initial commit

---
+++ build-test3-Desktop_Qt_5_9_1_GCC_64bit-Debug/CMakeFiles/pcl_openni2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pcl_openni2.dir/openni2_viewer.cpp.o"
+  "pcl_openni2.pdb"
+  "pcl_openni2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pcl_openni2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
